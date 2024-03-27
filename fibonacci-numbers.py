7f0c77e109e8375900b9c12117af5e6bb560ae14
Kari Smith <kariasmith@hotmail.com>
2024-03-26 18:46:06 -0600

    Update fibonacci-numbers.py

--- fibonacci-numbers.py
+++ fibonacci-numbers.py
@@ -7,3 +7,18 @@ In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the re
 with seed values : F0 = 0 and F1 = 1.
 """
 
+
+n = 10
+num1 = 0
+num2 = 1
+next_number = num2  
+count = 1
+ 
+while count <= n:
+    print(next_number, end=" ")
+    count += 1
+    num1, num2 = num2, next_number
+    next_number = num1 + num2
+print()
+
+# Output: 1 2 3 5 8 13 21 34 55 89 
