fc1617c76429d70b935a6f93400168731f66cbef
Kari Smith <kariasmith@hotmail.com>
2024-03-27 18:04:41 -0600

    Update fibonacci-numbers.py

--- fibonacci-numbers.py
+++ fibonacci-numbers.py
@@ -22,3 +22,33 @@ while count <= n:
 print()
 
 # Output: 1 2 3 5 8 13 21 34 55 89 
+
+"""
+Fibonacci numbers using Recursion.
+The term Recursion can be defined as the process of defining something in terms of itself. 
+In simple words, it is a process in which a function calls itself directly or indirectly. 
+"""
+
+def Fibonacci(n):
+ 
+    # Check if input is 0 then it will
+    # print incorrect input
+    if n < 0:
+        print("Incorrect input")
+ 
+    # Check if n is 0
+    # then it will return 0
+    elif n == 0:
+        return 0
+ 
+    # Check if n is 1,2
+    # it will return 1
+    elif n == 1 or n == 2:
+        return 1
+ 
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+ 
+ 
+# Driver Program
+print(Fibonacci(9))
